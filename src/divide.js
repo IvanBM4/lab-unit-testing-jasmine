163cfc7fc352ede18c6bf1628de4c548913d4fc5
Iván Blanco <ivanblanco4m@gmail.com>
2024-10-20 21:38:17 +0200

    complete lab finished

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,9 @@
-function divide () {
-       
+function divide(numOne, numTwo) {
+    if (typeof numOne != "number" || typeof numTwo != "number") {
+        return undefined
+    }
+    if (numOne == undefined || numTwo == undefined) {
+        return undefined;
+    }
+    return numOne / numTwo
 }
--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,32 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
 
-    })    
+        it("should be defined", () => {
+            expect(calculateArea).toBeDefined()
+        });
+        it("should take two numbers as arguments", () => {
+            expect(calculateArea.length).toBeDefined(2)
+
+        }); it("should return a number representing the area of a rectangle (the product of the two arguments)", () => {
+            expect(calculateArea(2, 4)).toEqual(8);
+            expect(calculateArea(4, 6)).toEqual(24);
+            expect(calculateArea(7, 9)).toEqual(63);
+
+        });
+        it("should return undefined in case any of the arguments is not provided", () => {
+            expect(calculateArea(1)).toEqual(undefined);
+            expect(calculateArea()).toEqual(undefined);
+            expect(calculateArea(undefined, 1)).toEqual(undefined);
+        });
+        it("should return undefined if any of the two arguments is not a number", () => {
+            expect(calculateArea("1", 2)).toEqual(undefined);
+            expect(calculateArea(3, "4")).toEqual(undefined);
+            expect(calculateArea("5", "6")).toEqual(undefined);
+        })
+    })
 })
 
-*/
